d9f3a917ae623ec1d8e9b541b4c459f1d5c4b3da
sameer1202 <kumarsameer1202@gmail.com>
2016-06-14 19:39:41 +0530

    Add files via upload

---
+++ DyslexiaDetector/app/src/main/java/android/support/design/widget/AppBarLayout.java
@@ -0,0 +1,14 @@
+package android.support.design.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by sameer work on 04-06-2016.
+ */
+public class AppBarLayout extends View {
+    public AppBarLayout(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+}
